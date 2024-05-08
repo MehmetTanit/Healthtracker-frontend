71289be67f334098657be04381d6f4da25dd5f5a
Muhammed Mehmet Tanit <Muhammed.Tanit@Student.HTW-Berlin.de>
2024-05-08 10:56:05 +0200

    kleineÄnderungen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HealthEntries from "@/components/HealthEntries.vue";
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HealthEntries />
   </main>
 </template>

e9a32dac93c95df3f84b35c9cdd7f30a484a2df1
Muhammed Mehmet Tanit <Muhammed.Tanit@Student.HTW-Berlin.de>
2024-06-24 14:05:41 +0200

    änderungen

--- src/components/SleepPattern.vue
+++ src/components/SleepPattern.vue
@@ -29,23 +29,11 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
+import { SleepPattern } from '@/model/SleepPattern';
 
 defineProps<{
   title: string
 }>()
-
-// Definieren der SleepPattern Klasse
-export class SleepPattern {
-  id: number;
-  dateRecorded: Date;
-  sleepDuration: number;
-
-  constructor(dateRecorded: Date, sleepDuration: number, id: number) {
-    this.id = id;
-    this.dateRecorded = dateRecorded;
-    this.sleepDuration = sleepDuration;
-  }
-}
 
 const sleepPatterns = ref<SleepPattern[]>([])
 const sleepDurationInput = ref<number>(0)

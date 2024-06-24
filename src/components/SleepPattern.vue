bf220f8841dcadf466db01f1f50db8cc4552a448
Muhammed Mehmet Tanit <Muhammed.Tanit@Student.HTW-Berlin.de>
2024-06-24 14:13:46 +0200

    änderungen neue

--- src/components/SleepPattern.vue
+++ src/components/SleepPattern.vue
@@ -1,31 +1,3 @@
-<template>
-  <div>
-    <h3>{{ title }}</h3>
-    <div>
-      <label for="sleepDuration">Sleep Duration:</label>
-      <input v-model="sleepDurationInput" id="sleepDuration" type="number">
-      <button @click="saveSleepPattern">Save</button>
-    </div>
-    <div>
-      <table v-if="sleepPatterns.length > 0">
-        <thead>
-        <tr>
-          <th>Date Recorded</th>
-          <th>Sleep Duration</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="pattern in sleepPatterns" :key="pattern.id">
-          <td>{{ pattern.dateRecorded }}</td>
-          <td>{{ pattern.sleepDuration }}</td>
-        </tr>
-        </tbody>
-      </table>
-      <p v-else>No sleep patterns recorded yet.</p>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
@@ -58,7 +30,7 @@ async function saveSleepPattern() {
 }
 
 async function loadSleepPatterns() {
-  const endpoint = `${baseUrl}/SleepPattern`
+  const endpoint = `${baseUrl}/SleepPattern/sleeppattern` // Adjusted endpoint for GET request
   try {
     const response = await axios.get<SleepPattern[]>(endpoint)
     sleepPatterns.value = response.data
@@ -72,16 +44,3 @@ onMounted(() => {
   loadSleepPatterns()
 })
 </script>
-
-<style scoped>
-h3 {
-  text-align: center;
-}
-table {
-  margin-left: auto;
-  margin-right: auto;
-}
-button {
-  color: blue;
-}
-</style>
